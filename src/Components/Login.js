98fc313c3d2f5b0d119d8f6c587d784dcf787f84
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-04-17 09:37:00 +0200

    finish registration

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -23,6 +23,13 @@ export default function Lab2() {
         password: "",
         confirmPassword: ""
     });
+    const [registrationError, setRegistrationError] = useState({
+        userNameError: "",
+        emailError: "",
+        phoneError: "",
+        passwordError: "",
+        confirmPasswordError: ""
+    });
 
     const handelLoginForm = (e) => {
         setLoginForm({
@@ -55,6 +62,56 @@ export default function Lab2() {
             ...registrationForm,
             [e.target.id]: e.target.value
         })
+        if (e.target.id === 'userName') {
+            setRegistrationError({
+                ...registrationError,
+                userNameError: e.target.value.length === 0 ? 'This field is required'
+                    : e.target.value.length < 3 ? "min length us 3 characters"
+                        : e.target.value.length > 10 ? "min length us 10 characters"
+                            : e.target.value.length > 100 ? "max length us 100 characters"
+                                : null,
+            })
+        }
+        if (e.target.id === 'email') {
+            setRegistrationError({
+                ...registrationError,
+                emailError: e.target.value.length === 0 ? 'This field is required'
+                    : e.target.value.length < 3 ? "min length us 3 characters"
+                        : e.target.value.length > 10 ? "min length us 10 characters"
+                            : e.target.value.length > 100 ? "max length us 100 characters"
+                                : null,
+            })
+        }
+        if (e.target.id === 'phone') {
+            setRegistrationError({
+                ...registrationError,
+                phoneError: e.target.value.length === 0 ? 'This field is required'
+                    : e.target.value.length < 3 ? "min length us 3 characters"
+                        : e.target.value.length > 10 ? "min length us 10 characters"
+                            : e.target.value.length > 100 ? "max length us 100 characters"
+                                : null,
+            })
+        }
+        if (e.target.id === 'password') {
+            setRegistrationError({
+                ...registrationError,
+                passwordError: e.target.value.length === 0 ? 'This field is required'
+                    : e.target.value.length < 3 ? "min length us 3 characters"
+                        : e.target.value.length > 10 ? "min length us 10 characters"
+                            : e.target.value.length > 100 ? "max length us 100 characters"
+                                : null,
+            })
+        }
+        if (e.target.id === 'confirmPassword') {
+            setRegistrationError({
+                ...registrationError,
+                confirmPasswordError: e.target.value.length === 0 ? 'This field is required'
+                    : e.target.value.length < 3 ? "min length us 3 characters"
+                        : e.target.value.length > 10 ? "min length us 10 characters"
+                            : e.target.value.length > 100 ? "max length us 100 characters"
+                                : null,
+            })
+        }
     }
     function chickIfLogin() {
         setISLogin(!isLogin)
@@ -105,17 +162,32 @@ export default function Lab2() {
                 ) : (
                     <div className="login-page">
                         <div className="form">
-                            <input type="text" placeholder="name" id="name" onChange={(e) => handelRegistrationForm(e)} />
-                            <input type="text" placeholder="email address" id="email" onChange={(e) => handelRegistrationForm(e)} />
-                            <input type="number" placeholder="phone" id="phone" onChange={(e) => handelRegistrationForm(e)} />
+                            <input type="text" placeholder="name" className={`form-control ${registrationError.userNameError ? 'border border-danger' : ""}`} id="userName" onChange={(e) => handelRegistrationForm(e)} />
+                            {registrationError.userNameError && (<div className='form-text text-danger error'>
+                                {registrationError.userNameError}
+                            </div>)}
+                            <input type="text" placeholder="email address" className={`form-control ${registrationError.emailError ? 'border border-danger' : ""}`} id="email" onChange={(e) => handelRegistrationForm(e)} />
+                            {registrationError.emailError && (<div className='form-text text-danger error'>
+                                {registrationError.emailError}
+                            </div>)}
+                            <input type="number" placeholder="phone" className={`form-control ${registrationError.phoneError ? 'border border-danger' : ""}`} id="phone" onChange={(e) => handelRegistrationForm(e)} />
+                            {registrationError.phoneError && (<div className='form-text text-danger error'>
+                                {registrationError.phoneError}
+                            </div>)}
                             <div className="input-group my-2">
-                                <input id='password' className="form-control" type="password" placeholder="password" onChange={(e) => handelRegistrationForm(e)} />
+                                <input id='password' className={`form-control ${registrationError.passwordError ? 'border border-danger' : ""}`} type="password" placeholder="password" onChange={(e) => handelRegistrationForm(e)} />
                                 <i onClick={() => showPassword("password")} className={togglePassword}></i>
                             </div>
+                            {registrationError.passwordError && (<div className='form-text text-danger error'>
+                                {registrationError.passwordError}
+                            </div>)}
                             <div className="input-group my-2">
-                                <input id='confirmPassword' className="form-control" type="password" placeholder="confirm password" onChange={(e) => handelRegistrationForm(e)} />
+                                <input id='confirmPassword' className={`form-control ${registrationError.confirmPasswordError ? 'border border-danger' : ""}`} type="password" placeholder="confirm password" onChange={(e) => handelRegistrationForm(e)} />
                                 <i onClick={() => showPassword("confirmPassword", true)} className={confTogglePassword}></i>
                             </div>
+                            {registrationError.confirmPasswordError && (<div className='form-text text-danger error'>
+                                {registrationError.confirmPasswordError}
+                            </div>)}
                             <button onClick={() => goToHome(true)}>create</button>
                             <p className="message">Already registered? <span onClick={() => chickIfLogin()}>Sign In</span></p>
                         </div>
